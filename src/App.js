72c091cd5bbd79315cc7ba233a2ea1d23e63f05c
Balaganesh003 <k.balaganesh26@gmail.com>
2022-12-19 20:40:14 +0530

    completed the header

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
     <AnimatePresence exitBeforeEnter>
       <div className="w-screen h-auto flex flex-col bg-primary ">
         <Header />
-        <main className="mt-24 p-8 w-full">
+        <main className="mt-16 md:mt-24 p-8  w-full">
           <Routes>
             <Route path="/*" element={<Home />} />
             <Route path="/createItem" element={<CreateContainer />} />
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,33 +1,35 @@
 import React from 'react';
 import Logo from '../../img/logo.png';
+import Avatar from '../../img/avatar.png';
 import { AiFillShopping } from 'react-icons/ai';
+import { MdAdd, MdLogout } from 'react-icons/md';
 
 import { getAuth, signInWithPopup, GoogleAuthProvider } from 'firebase/auth';
 import { app } from '../../firebase.config';
-import { MdAdd, MdLogout } from 'react-icons/md';
 
-// import { BiShoppingBag } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
-import Avatar from '../../img/avatar.png';
 import { motion } from 'framer-motion';
+
+import { Link } from 'react-router-dom';
 import { useDispatch } from 'react-redux';
 import { authActions } from '../../store/auth-slice';
 import { uiActions } from '../../store/ui-slice';
 import { useSelector } from 'react-redux';
 
 const Header = () => {
+  const dispatch = useDispatch();
   const firebaseAuth = getAuth(app);
   const provider = new GoogleAuthProvider();
-  const dispatch = useDispatch();
   const user = useSelector((state) => state.auth.user);
   const isMenuOpen = useSelector((state) => state.ui.isMenuOpen);
 
   const login = async () => {
-    const {
-      user: { refreshToken, providerData },
-    } = await signInWithPopup(firebaseAuth, provider);
-    dispatch(authActions.setUser(providerData[0]));
-    localStorage.setItem('user', JSON.stringify(providerData[0]));
+    if (!user) {
+      const {
+        user: { providerData },
+      } = await signInWithPopup(firebaseAuth, provider);
+      dispatch(authActions.setUser(providerData[0]));
+      localStorage.setItem('user', JSON.stringify(providerData[0]));
+    }
   };
 
   const logout = () => {
@@ -67,6 +69,7 @@ const Header = () => {
               Contact
             </li>
           </motion.ul>
+
           <div className="relative flex items-center justify-center">
             <AiFillShopping className="text-textColor text-2xl cursor-pointer" />
             <div className=" absolute -top-2 -right-2 flex items-center justify-center w-5 h-5 rounded-full bg-cartNumBg">
@@ -105,13 +108,10 @@ const Header = () => {
           </div>
         </div>
       </div>
-      {/* Mobile */}
-      <div className="flex items-center justify-between md:hidden w-full h-full ">
-        <Link to={'/'} className="flex items-center gap-2">
-          <img src={Logo} alt="logo" className="w-8 object-cover" />
-          <p className="text-headingColor text-xl font-bold">City</p>
-        </Link>
 
+      {/* Mobile */}
+
+      <div className="flex items-center justify-between md:hidden w-full h-full ">
         <div className="relative">
           <motion.img
             whileTap={{ scale: 0.6 }}
@@ -125,7 +125,7 @@ const Header = () => {
               initial={{ opacity: 0, scale: 0.6 }}
               animate={{ opacity: 1, scale: 1 }}
               exit={{ opacity: 0, scale: 0.6 }}
-              className="w-40 bg-blue-300 shadow-xl flex flex-col  top-12 right-0 rounded-lg absolute">
+              className="w-40 bg-blue-300 shadow-xl flex flex-col  top-12 left-0 rounded-lg absolute">
               {user && user.email === 'k.balaganesh26@gmail.com' && (
                 <Link to={'/createItem'}>
                   <p className="px-4 py-2 flex items-center gap-3 cursor-pointer hover:bg-blue-400 transition-all duration-100 ease-in-out text-textColor text-base hover:text-headingColor rounded-lg">
@@ -154,6 +154,17 @@ const Header = () => {
               </p>
             </motion.div>
           )}
+        </div>
+
+        <Link to={'/'} className="flex items-center gap-2">
+          <img src={Logo} alt="logo" className="w-8 object-cover" />
+          <p className="text-headingColor text-xl font-bold">City</p>
+        </Link>
+        <div className="relative flex items-center justify-center">
+          <AiFillShopping className="text-textColor text-2xl cursor-pointer" />
+          <div className=" absolute -top-2 -right-2 flex items-center justify-center w-5 h-5 rounded-full bg-cartNumBg">
+            <p className=" text-white text-xs font-semibold">2</p>
+          </div>
         </div>
       </div>
     </header>
