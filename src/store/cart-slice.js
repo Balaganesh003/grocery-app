64dc7c81c0f0c82a79377b47e9ebdfc487e616b5
Balaganesh003 <k.balaganesh26@gmail.com>
2022-12-26 04:37:44 +0530

    added addTocart and removeFromCart functionality

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -14,8 +14,35 @@ const cartSlice = createSlice({
     toggleCart(state) {
       state.isCartOpen = !state.isCartOpen;
     },
-    addItemToCart(state, action) {},
-    removeItemFromCart(state, action) {},
+    addItemToCart(state, action) {
+      const newItem = action.payload;
+
+      const existingItem = state.items.find((item) => item.id === newItem.id);
+      if (existingItem) {
+        existingItem.quantity++;
+        existingItem.totalPrice += newItem.price;
+      } else {
+        state.items.push({
+          id: newItem.id,
+          name: newItem.name,
+          price: newItem.price,
+          quantity: 1,
+          totalPrice: newItem.price,
+        });
+        state.totalQuantity++;
+      }
+    },
+    removeItemFromCart(state, action) {
+      const id = action.payload;
+      const existingItem = state.items.find((item) => item.id === id);
+      if (existingItem.quantity === 1) {
+        state.itemsList = state.items.filter((item) => item.id !== id);
+        state.totalQuantity--;
+      } else {
+        existingItem.quantity--;
+        existingItem.totalPrice -= existingItem.price;
+      }
+    },
   },
 });
 
