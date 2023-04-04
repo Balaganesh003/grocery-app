f18baf870c378cc2fe9bf4f602b02335c7c13c86
Balaganesh003 <k.balaganesh26@gmail.com>
2023-04-04 16:48:28 +0530

    updated the firebase.config.js file with the correct values

--- src/firebase.config.js
+++ src/firebase.config.js
@@ -3,15 +3,13 @@ import { getFirestore } from 'firebase/firestore';
 import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDymRJ0div9TVvl4mxy5MZ_kAqug4tXTD4',
-  authDomain: 'grocery-app-28105.firebaseapp.com',
-  databaseURL:
-    'https://grocery-app-28105-default-rtdb.asia-southeast1.firebasedatabase.app',
-  projectId: 'grocery-app-28105',
-  storageBucket: 'grocery-app-28105.appspot.com',
-  messagingSenderId: '576568786877',
-  appId: '1:576568786877:web:3ce0abb1e8ce0201b01cd4',
-  measurementId: 'G-1HKPJ9G3JC',
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DB_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 const app = getApps.length > 0 ? getApp() : initializeApp(firebaseConfig);
